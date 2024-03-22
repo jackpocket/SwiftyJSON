d53dc40dda0d13312fc39b2c954be1f4606b7231
Joshua Alexander <joshua@jackpocket.com>
2024-03-21 22:10:13 -0400

    TEP-2256 Adds Privacy Manifest file and updates Package.swift

--- Package.swift
+++ Package.swift
@@ -1,14 +1,26 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
     name: "SwiftyJSON",
     products: [
-        .library(name: "SwiftyJSON", targets: ["SwiftyJSON"])
+        .library(
+            name: "SwiftyJSON",
+            targets: ["SwiftyJSON"]
+        )
     ],
     targets: [
-        .target(name: "SwiftyJSON", dependencies: []),
-        .testTarget(name: "SwiftJSONTests", dependencies: ["SwiftyJSON"])
+        .target(
+            name: "SwiftyJSON",
+            dependencies: [],
+            resources: [
+                .copy("PrivacyInfo.xcprivacy")
+            ]
+        ),
+        .testTarget(
+            name: "SwiftJSONTests",
+            dependencies: ["SwiftyJSON"]
+        )
     ],
     swiftLanguageVersions: [.v5]
 )
